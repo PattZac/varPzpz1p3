4eb11e2fc333087a4730fbe399018b224343870f
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 15:23:22 +0100

    Add files via upload

---
+++ varPzpz1p3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varPzpz1p3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite neku rečenicu: ");
+            string s = Console.ReadLine();
+            Console.WriteLine(s.Substring(0,s.IndexOf(' '))+
+                s.Substring(s.LastIndexOf(' ') , s.Length-s.LastIndexOf(' ')));
+            Console.ReadKey();
+        }
+    }
+}
